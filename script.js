a4184a45ed9d8ef69fab7a95beef408c3fa6f78e
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-06-19 12:17:37 +0200

    Create grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16; i++) {
+  let rowWrapper = document.createElement("div");
+  rowWrapper.style.display = "flex";
+  rowWrapper.style.flexDirection = "row";
+  rowWrapper.style.width = "960px";
+  rowWrapper.style.height = `${960 / 16}px`;
+  for (let j = 0; j < 16; j++) {
+    let gridCell = document.createElement("div");
+    gridCell.style.width = `${960 / 16}px`;
+    gridCell.style.height = `${960 / 16}px`;
+    gridCell.style.border = "1px solid black";
+    rowWrapper.appendChild(gridCell);
+  }
+
+  gridContainer.appendChild(rowWrapper);
+}
